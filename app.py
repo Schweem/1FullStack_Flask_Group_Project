426f6b6aa180e71d18aa220756412d73cbd28985
gsaluncf <gsalu@ncf.edu>
2023-10-04 11:28:34 -0400

    10-3 in class

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import sqlite3
 
 from flask import Flask, render_template, request, session , redirect, url_for
 from flask_session import Session
 app = Flask(__name__)
 app.secret_key = 'some_secret_key'  # Change this to a more secure key in a real application
 # Configure session to use filesystem
+
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True
 app.config['SESSION_KEY_PREFIX'] = 'login_session:'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(app)
@@ -15,12 +16,20 @@ Session(app)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # For now, we're not checking the credentials, just setting the session
-        session['authenticated']= True
-        session['name'] = request.form['username']
-        return redirect(url_for ('index'))  # Redirect to the main page after login
-    return render_template('index.html')
+        con = sqlite3.connect('SarasotaBikeGang.db')
+        cur = con.cursor()
+        cur.execute("SELECT * from users WHERE username=? AND password=?", (request.form['username'], request.form['password']))
 
+        if cur.fetchone() is None:
+            con.close()
+            return render_template('index.html', message='Invalid credentials')
+        else:
+            # For now, we're not checking the credentials, just setting the session
+            con.close()
+            session['authenticated']= True
+            session['name'] = request.form['username']
+            return redirect(url_for ('index'))  # Redirect to the main page after login
+    return render_template('index.html')
 
 @app.route('/logout')
 def logout():
@@ -33,8 +42,8 @@ def is_authenticated():
 
 @app.route('/')
 def index():
-    if not is_authenticated():
-        return redirect(url_for('login'))
+    # if not is_authenticated():
+    #     return redirect(url_for('login'))
     # Sample data for demonstration
     data = {
         'station_id': [1, 2, 3],
@@ -43,8 +52,13 @@ def index():
         'latitude': [34.0522, 36.7783, 40.7128],
         'longitude': [-118.2437, -119.4179, -74.0060]
     }
-    df = pd.DataFrame(data)
 
+    df = pd.DataFrame(data)
+    print (df)
+    con = sqlite3.connect('SarasotaBikeGang.db')
+    df = pd.read_sql_query("Select station_id, name, status, latitude, longitude from bikeshare_stations", con)
+    print(df.head())
+    con.close()
     name=session.get('name', 'Unknown')
 
     return render_template('availability.html', name=name, stations=df)
@@ -59,21 +73,26 @@ def bikeAvailbilty(station_id):
         'status_code': [1, 2, 1],
         'comment': ['Good condition', 'Needs tire replacement', 'New bike']
     }
+    conn = sqlite3.connect('SarasotaBikeGang.db')
+    sqlString = "select b.bike_id, b.bike_type, b.status_code, s.name, s.status from bikes b join bikeshare_stations s on b.last_station_id = s.station_id where b.last_station_id = " + station_id
+    print (sqlString)
+    df = pd.read_sql_query(sqlString, conn)
+    # bikes_df = pd.DataFrame(data)
 
-    bikes_df = pd.DataFrame(data)
 
-    return bikes_df
+    return df
 
 
 @app.route('/checkAvailability' , methods=['GET', 'POST'])
 def checkAvailability():
-    choice = request.args.get('station_id')
-    print(choice)
+    choice = request.form['location']
+    print("what was chosen: " , choice)
     bikes = bikeAvailbilty(choice)
     print (bikes)
     if len (bikes) > 0:
         print("Bike is available")
         return render_template('booking.html', name=session.get('name', 'Unknown'), message="Bikes are available", bikes=bikes)
+
 if __name__ == "__main__":
     app.run(debug=True)
     # print(testvale)
